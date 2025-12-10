71024da48d33cfe6e3f6d006121dfb9ccd59827a
Safmica <safmikasa@gmail.com>
2025-12-10 16:34:00 +0700

    fix: try to empty the workspace

--- src/main/java/com/safmica/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.safmica;
-
-public class Launcher {
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
--- src/main/java/com/safmica/model/GameAnswer.java
+++
@@ -1,9 +0,0 @@
-package com.safmica.model;
-
-public class GameAnswer {
-    public String answer;
-    public double x;
-    public String username;
-    public boolean status;
-    public int round;
-}
